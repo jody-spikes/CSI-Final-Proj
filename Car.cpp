39194a4398c629b0d9145398916a4d68b8965193
jody-spikes <166843658+jody-spikes@users.noreply.github.com>
2025-11-23 22:10:40 -0500

    Added wheels to Car.cpp
    made some black squares in the corner of the car that kinda resemble wheels

--- Car.cpp
+++ Car.cpp
@@ -7,6 +7,13 @@
 
 #include <stdio.h>
 #include "Car.h"
+void drawRect(int x, int y, int width, int height, color c, SDL_Plotter& g){
+    for(int row = y; row < y + height; row++){
+        for(int col = x; col < x + width; col++){
+            g.plotPixel(row, col, c);
+        }
+    }
+}
 
 //Move
 void Car::move(char direction){
@@ -23,12 +30,20 @@ void Car::move(char direction){
 }
 
 void Car::draw(SDL_Plotter& g){
+    //
+    int whlSize = _size/5 + 2;
+    color black(0,0,0);
+    
     erase(g);
-    for (int y = _loc.y - _size / 2; y <= _loc.y + _size / 2; y++){
-        for(int x = _loc.x - _size / 2; x <= _loc.x + _size / 2; x++) {
-            g.plotPixel(x,y,_color);
-        }
-    }
+    //car base
+    drawRect( _loc.y - _size/2,_loc.x - _size/2, _size, _size, _color, g);
+    
+    //wheels
+    drawRect( _loc.y - _size/2,_loc.x - _size/2, whlSize, whlSize, black, g);
+    drawRect( _loc.y + _size/2 - 5, _loc.x - _size/2, whlSize, whlSize, black, g);
+    drawRect( _loc.y - _size/2,_loc.x + _size/2 - whlSize + 1,whlSize,whlSize,black,g);
+    drawRect( _loc.y + _size/2 - whlSize + 1,_loc.x + _size/2 - whlSize + 1,whlSize,whlSize,black,g);
+    
 }
 
 
